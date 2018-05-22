8df11fcea07fbdfd0e27001204f0a8c544a0728c
wykys <vvykys@gmail.com>
2018-05-22 20:12:00 +0200

    added uart api

---
+++ nucleo/Inc/uart.hpp
@@ -0,0 +1,34 @@
+#ifndef __UART_INCLUDED__
+#define __UART_INCLUDED__
+
+#include "stm32f3xx_hal.h"
+
+#define RX_BUFFER_SIZE 1
+
+class Uart
+{
+private:
+    UART_HandleTypeDef *uart;
+    bool rx_flag;
+public:
+    uint8_t rx_buffer[RX_BUFFER_SIZE];
+
+    Uart(UART_HandleTypeDef *uart);
+
+    inline void send_byte(uint8_t byte)
+    {
+        HAL_UART_Transmit(uart, &byte, 1, 0xFF);
+    }
+
+    inline void receive_it(void)
+    {
+        HAL_UART_Receive_IT(uart, rx_buffer, RX_BUFFER_SIZE);
+    }
+
+    void set_rx_flag(void);
+    void clear_rx_flag(void);
+    bool get_rx_flag(void);
+    void print(char str[]);
+};
+
+#endif // ifndef __UART_INCLUDED__
